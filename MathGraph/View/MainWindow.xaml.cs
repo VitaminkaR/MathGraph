d1a4863ba478357eb5be2069f433fd3487ee8069
VladimirR <rvitaminka20@gmail.com>
2024-06-06 14:36:10 +0300

    Создание VM

--- MathGraph/View/MainWindow.xaml.cs
+++ MathGraph/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using MathGraph.ViewModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,8 @@ namespace MathGraph
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new ApplicationViewModel();
         }
     }
 }
