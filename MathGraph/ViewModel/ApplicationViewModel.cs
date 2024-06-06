355b2aab1828141f62d83a18927a955a166b4f82
VladimirR <rvitaminka20@gmail.com>
2024-06-07 01:04:52 +0300

    ViewModel  работа с характеристиками AreaDrawer

--- MathGraph/ViewModel/ApplicationViewModel.cs
+++ MathGraph/ViewModel/ApplicationViewModel.cs
@@ -1,16 +1,59 @@
-﻿using System;
+﻿using MathGraph.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MathGraph.ViewModel
 {
-    internal class ApplicationViewModel
+    internal class ApplicationViewModel : INotifyPropertyChanged
     {
-        public ApplicationViewModel() 
-        {
+        private ISolver m_Solver;
 
+        public double DrawAreaXMinRange
+        {
+            get => m_Solver.AreaRange.X;
+            set
+            {
+                m_Solver.AreaRange = new Vector2((float)value, m_Solver.AreaRange.Y);
+                OnPropertyChanged("DrawAreaXMinRange");
+            }
+        }
+
+        public double DrawAreaXMaxRange
+        {
+            get => m_Solver.AreaRange.Y;
+            set
+            {
+                m_Solver.AreaRange = new Vector2(m_Solver.AreaRange.X, (float)value);
+                OnPropertyChanged("DrawAreaXMaxMRange");
+            }
+        }
+
+        public double Accuracy
+        {
+            get => m_Solver.Accuracy;
+            set
+            {
+                m_Solver.Accuracy = value;
+                OnPropertyChanged("Accuracy");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            m_Solver = new Solver();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
         }
     }
 }
