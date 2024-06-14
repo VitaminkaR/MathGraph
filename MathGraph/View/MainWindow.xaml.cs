d03e3ca559d84b72478414bae77bd0db77c70e23
VladimirR <rvitaminka20@gmail.com>
2024-06-14 21:19:58 +0300

    Рисование осей

--- MathGraph/View/MainWindow.xaml.cs
+++ MathGraph/View/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace MathGraph
         {
             InitializeComponent();
 
-            
+
 
             DataContext = new ApplicationViewModel();
             ((ApplicationViewModel)DataContext).OnGraphSolved += DrawGraph;
@@ -37,6 +37,7 @@ namespace MathGraph
         {
             Canvas_DrawArea.Children.Clear();
 
+            // основная линия графика
             Polyline polyline = new Polyline();
             polyline.Stroke = Brushes.Red;
             polyline.StrokeThickness = 2;
@@ -56,14 +57,54 @@ namespace MathGraph
                 if (val < ymin)
                     ymin = val;
             }
-            
+
             double xmin = double.Parse(TB_MinX.Text.Replace(".", ",")); // минимальное значение х на промежутке
             double xmax = double.Parse(TB_MaxX.Text.Replace(".", ",")); // максимальное значение х на промежутке
 
-            int centerx = width / 2; // центр области х
             double xproj = width / (xmax - xmin); // размер пикселя на значение
+            double yproj = height / (xmax - xmin); // размер пикселя на значение
+            int centerx = (int)(width / 2 - (xmax + xmin) / 2 * xproj); // центр области х
             int centery = height / 2; // центр области у
-            double yproj = height / (xmax -xmin); // размер пикселя на значение
+
+            // отрисовка осей
+            // x
+            Line line = new Line(); line.Stroke = Brushes.Black; line.StrokeThickness = 2;
+            line.X1 = 0; line.X2 = width; line.Y1 = centery; line.Y2 = centery;
+            Canvas_DrawArea.Children.Add(line);
+            for (int i = (int)xmin; i <= xmax; i++)
+            {
+                line = new Line(); line.Stroke = Brushes.Black; line.StrokeThickness = 2;
+                line.X1 = (int)(i * xproj) + centerx; line.X2 = (int)(i * xproj) + centerx; line.Y1 = centery - 2; line.Y2 = centery + 2;
+
+                // метка для значения x
+                Label l = new Label(); l.Content = i.ToString();
+                // вычисляем размер текста до рендера, чтобы отцентрировать метку
+                l.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                Size s = l.DesiredSize;
+                l.Margin = new Thickness((int)(i * xproj) + centerx - s.Width / 2, centery + 2, 0, 0);
+
+                Canvas_DrawArea.Children.Add(line);
+                Canvas_DrawArea.Children.Add(l);
+            }
+            // y
+            line = new Line(); line.Stroke = Brushes.Black; line.StrokeThickness = 2;
+            line.X1 = centerx; line.X2 = centerx; line.Y1 = 0; line.Y2 = height;
+            Canvas_DrawArea.Children.Add(line);
+            for (int i = (int)xmin; i <= xmax; i++)
+            {
+                line = new Line(); line.Stroke = Brushes.Black; line.StrokeThickness = 2;
+                line.X1 = centerx - 2; line.X2 = centerx + 2; line.Y1 = (int)(i * -1 * yproj) + centery; line.Y2 = (int)(i * -1 * yproj) + centery;
+
+                // метка для значения x
+                Label l = new Label(); l.Content = (i * -1).ToString();
+                // вычисляем размер текста до рендера, чтобы отцентрировать метку
+                l.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                Size s = l.DesiredSize;
+                l.Margin = new Thickness(centerx + 2, (int)(i * yproj) + centery - s.Height / 2, 0, 0);
+
+                Canvas_DrawArea.Children.Add(line);
+                Canvas_DrawArea.Children.Add(l);
+            }
 
             for (int i = 0; i < points.Count; i++)
             {
@@ -71,6 +112,7 @@ namespace MathGraph
                 double cy = points[i].Y;
                 cy *= -1; // координаты элементов в wpf имеет противоположную направленность относительно декартовых координат
 
+                // если значению отсутствует делаем разрыв
                 if (double.IsNaN(cx) && !m_MathGap)
                 {
                     m_MathGap = true;
@@ -80,15 +122,15 @@ namespace MathGraph
                     polyline.StrokeThickness = 2;
                 }
 
-                if(m_MathGap && !double.IsNaN(cx))
+                if (m_MathGap && !double.IsNaN(cx))
                     m_MathGap = false;
 
                 int x1coord = (int)(cx * xproj) + centerx;
                 int y1coord = (int)(cy * yproj) + centery;
 
-                if (y1coord >= height || y1coord <= ofsy)
+                if (y1coord >= height || y1coord <= 0)
                     continue;
-                if (x1coord >= width || x1coord <= ofsx)
+                if (x1coord >= width || x1coord <= 0)
                     continue;
 
                 polyline.Points.Add(new Point(x1coord, y1coord));
